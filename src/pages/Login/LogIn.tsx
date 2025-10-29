d260333bffcfbe68df65c80a07f02a977bd8f832
sheranbuckmangit <sherantuanbuckman@gmail.com>
2025-10-29 23:40:50 +0530

    feat: fix / mistake

--- src/pages/Login/LogIn.tsx
+++ src/pages/Login/LogIn.tsx
@@ -16,7 +16,7 @@ const Login: React.FC = () => {
 
   const handleLogin = async () => {
     try {
-      const res = await fetch(`${baseURI}/login`, {
+      const res = await fetch(`${baseURI}login`, {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ name: email, password }), // match your backend
